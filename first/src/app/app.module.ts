7867285e5ab055b271e2d1ec39e56175165be91b
arman <Arman15411@mastek.com>
2022-03-15 09:47:39 +0530

    forth_commit

--- first/src/app/app.module.ts
+++ first/src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PatientSectionComponent } from './patient-section/patient-section.component';
+import { DoctorSectionComponent } from './doctor-section/doctor-section.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
 
+
+var routelist : Routes =[
+  {path:"home",component:HomeComponent},
+  {path:"patient-section",component:PatientSectionComponent},
+  {path:"doctor-section",component:DoctorSectionComponent},
+  {path:"about-us",component:AboutUsComponent},
+  {path:"contact-us",component:ContactUsComponent}
+  
+]
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    PatientSectionComponent,
+    DoctorSectionComponent,
+    AboutUsComponent,
+    ContactUsComponent,
    
    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routelist)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ first/src/app/patient-section/patient-section.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PatientSectionComponent } from './patient-section.component';
+
+describe('PatientSectionComponent', () => {
+  let component: PatientSectionComponent;
+  let fixture: ComponentFixture<PatientSectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PatientSectionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PatientSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
