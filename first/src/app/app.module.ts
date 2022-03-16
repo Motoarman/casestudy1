d3f6cebf395c02025f8806892799b0fccab1b19b
arman <Arman15411@mastek.com>
2022-03-16 20:02:38 +0530

    app code added to navbar compo

--- first/src/app/app.module.ts
+++ first/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { OrthopaedicsComponent } from './orthopaedics/orthopaedics.component';
 import { NeurologyComponent } from './neurology/neurology.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 var routelist: Routes = [
@@ -37,6 +38,7 @@ var routelist: Routes = [
     NeurologyComponent,
     HeaderComponent,
     FooterComponent,
+    NavbarComponent,
     
   ],
   imports: [BrowserModule, RouterModule.forRoot(routelist),FormsModule],
