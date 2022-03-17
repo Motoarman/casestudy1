d39fcb4582a854967189dba5b4f553e1bd56b0be
Anuj Patil <anujptl8@gmail.com>
2022-03-17 06:06:17 +0530

    added extra treatment component. Oncology.

---
+++ first/src/app/oncology/oncology.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-oncology',
+  templateUrl: './oncology.component.html',
+  styleUrls: ['./oncology.component.css']
+})
+export class OncologyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
